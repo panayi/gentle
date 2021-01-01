6597f81526d79b6dd6b51566ac3cb5c964805ee1
Panagiotis Panagi <ppanagi@dynopia.com>
2021-01-02 00:21:32 +0200

    Fix build

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -7,6 +7,6 @@ make
 ./extras/install_openblas.sh
 cd ../src
 # make clean (sometimes helpful after upgrading upstream?)
-./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
+./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install  --fst-root=../tools/openfst-1.6.5/src
 make depend
 cd ../../
